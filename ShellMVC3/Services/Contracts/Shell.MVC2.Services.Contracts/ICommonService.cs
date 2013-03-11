7e78c44488c0b42933cb4139b4afc61df5801c36
ola lawal <ola_lawal@yahoo.com>
2013-03-11 18:52:20 -0500

    working on adding common service for stuff suchs as
    paging calculations  maybe

---
+++ ShellMVC3/Services/Contracts/Shell.MVC2.Services.Contracts/ICommonService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel.Web;
+using System.ServiceModel;
+using System.Text;
+using System.Web.Security;
+using Dating.Server.Data.Models;
+using Shell.MVC2.Domain.Entities.Anewluv;
+using Shell.MVC2.Domain.Entities.Anewluv.ViewModels;
+
+
+namespace Shell.MVC2.Services.Contracts
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface ICommonService
+    {
+
+        [WebGet]
+        [OperationContract]
+        List<systempagesetting> getsystempagesettinglist();
+        [WebGet]
+        [OperationContract]
+        List<lu_gender> getgenderlist();
+     
+
+
+    }
+}
---
+++ ShellMVC3/Services/Shell.MVC2.Services.Common/CommonService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using Shell.MVC2.Services.Contracts;
+using Shell.MVC2.Interfaces;
+
+
+
+using System.Web;
+using System.Net;
+
+using Shell.MVC2.Domain.Entities.Anewluv;
+using Shell.MVC2.Domain.Entities.Anewluv.ViewModels;
+using System.ServiceModel.Activation;
+
+namespace Shell.MVC2.Services.Common
+{
+
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "MembersService" in both code and config file together.
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.PerCall)]
+    public class CommonService : ICommonService 
+    {
+
+
+        private ICommonRepository  _lookuprepository;
+        // private string _apikey;
+
+        public CommonService(ICommonRepository lookuprepository)
+        {
+            _lookuprepository = lookuprepository;
+            //  _apikey  = HttpContext.Current.Request.QueryString["apikey"];
+            //   throw new System.ServiceModel.Web.WebFaultException<string>("Invalid API Key", HttpStatusCode.Forbidden);
+
+        }
+
+
+
+
+
+    
+
+     
+     
+     
+    }
+
+}
