31481d6491d96bc513131a81d0c162c9101ba435
ola lawal <ola_lawal@yahoo.com>
2013-03-14 18:53:24 -0500

    Working on ISO date mapping

---
+++ ShellMVC3/Infrastructure/MessageFormatters/JsonNetMessageFormatter/Extentions/NewtonSoftJsonBehaviorExtention.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel.Configuration;
+
+namespace JsonNetMessageFormatter.Extensions
+
+{
+    public class NewtonsoftJsonBehaviorExtension : BehaviorExtensionElement
+    {
+        public override Type BehaviorType
+        {
+            get { return typeof(NewtonsoftJsonBehavior); }
+        }
+
+        protected override object CreateBehavior()
+        {
+            return new NewtonsoftJsonBehavior();
+        }
+    }
+}
