fc6437affcd445784be0a872866650d127346a35
olalawal <ola_lawal@yahoo.com>
2014-12-28 05:28:32 -0600

    renamed

---
+++ Nmedia.Anewluv.Services/Domain/Data/Anewluv.Domain.Data/Anewluv/ViewModels/SearchSettings/SearchSettingsModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Collections;
+using System.Runtime.Serialization;
+
+
+namespace Anewluv.Domain.Data.ViewModels
+{
+
+    //constants for page indexes
+    
+        
+    //populates and updates a search page similar to edit
+    [DataContract]
+    public class SearchSettingsModel
+    {
+        //basic settings  
+        [DataMember]
+        public BasicSearchSettingsModel basicsearchsettings { get; set; }
+        [DataMember]
+        public AppearanceSearchSettingsModel appearancesearchsettings { get; set; }
+       [DataMember]
+        public LifeStyleSearchSettingsModel lifestylesearchsettings { get; set; }
+           [DataMember]
+        public CharacterSearchSettingsModel charactersearchsettings { get; set; }
+        //index of what page we are looking at i.e we want to split up this model into diff partial views
+        [DataMember]
+        public int profileid { get; set; }
+        [DataMember]
+        public int searchid { get; set; }
+        [DataMember]
+        public string searchname { get; set; }
+        [DataMember]
+        public int searchrank { get; set; }
+        [DataMember]
+        public int viewindex { get; set; }
+        public bool postalcodestatus { get; set; }
+        [DataMember]
+        public bool isfullediting { get; set; }
+
+        public  List<string> currenterrors = new List<string>();
+    }
+}
