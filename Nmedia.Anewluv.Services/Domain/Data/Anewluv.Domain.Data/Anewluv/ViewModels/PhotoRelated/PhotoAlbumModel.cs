454748bd7ac6601370a5b64aa3a8a60182022f69
olalawal <ola_lawal@yahoo.com>
2015-04-13 20:02:15 -0500

    added

---
+++ Nmedia.Anewluv.Services/Domain/Data/Anewluv.Domain.Data/Anewluv/ViewModels/PhotoRelated/PhotoAlbumModel.cs
@@ -0,0 +1,20 @@
+﻿using Nmedia.Infrastructure.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anewluv.Domain.Data.ViewModels
+{
+    [DataContract]
+    public class PhotoAlbumModel
+    {
+        public int id { get; set; }
+        public string description { get; set; }
+        public bool active { get; set; }
+        public List<listitem> securitylevels  { get; set; }
+       
+    }
+}
