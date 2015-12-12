71830b27c949803271655087b3d321703367a1b7
olalawal <ola_lawal@yahoo.com>
2015-12-11 23:36:57 -0600

    removed old code

--- Nmedia.Anewluv.Services/Services/Anewluv.Services.Authentication/AuthenticationService.cs
+++ Nmedia.Anewluv.Services/Services/Anewluv.Services.Authentication/AuthenticationService.cs
@@ -2717,19 +2717,7 @@ namespace Anewluv.Services.Authentication
 
 
 
-        [Serializable]
-        public class MyException : Exception
-        {
-            public MyException() { }
-            public MyException(string message) : base(message) { }
-            public MyException(string message, Exception inner) : base(message, inner) { }
-            protected MyException(
-              System.Runtime.Serialization.SerializationInfo info,
-              System.Runtime.Serialization.StreamingContext context)
-                : base(info, context) { }
-        }
-
-
+       
 
 
 
